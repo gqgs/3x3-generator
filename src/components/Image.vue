49d5cff1a2672338bf8859403101b426a641a0e6
Guilherme <gqgs@protonmail.com>
2021-04-04 05:03:52 -0300

    smartcrop

--- src/components/Image.vue
+++ src/components/Image.vue
@@ -1,6 +1,6 @@
 <template>
 <a href="#" @click.prevent="showSearch({id: id, imageSetter: setImage})">
-  <img v-if="image" :src="image" />
+  <canvas v-if="image" ref="canvas" />
   <span v-else>Select image</span>
 </a>
 </template>
@@ -8,6 +8,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { mapActions } from 'vuex'
+import smartcrop from 'smartcrop'
 
 export default defineComponent({
   data () {
@@ -26,6 +27,18 @@ export default defineComponent({
       'showSearch'
     ]),
     setImage (image: string) {
+      const img = new Image()
+      img.onload = async () => {
+        const res = await smartcrop.crop(img, { height: 200, width: 200 })
+        const canvas = (this.$refs.canvas as HTMLCanvasElement)
+        canvas.width = 200
+        canvas.height = 200
+        const ctx = canvas.getContext('2d')
+        const { x, y, width, height } = res.topCrop
+        console.log(ctx?.drawImage(img, x, y, width, height, 0, 0, 200, 200))
+      }
+      img.crossOrigin = 'Anonymous'
+      img.src = image
       this.image = image
     }
   }
