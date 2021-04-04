0a1055d6dd997ef114ba31de51d22741237e5028
Guilherme <gqgs@protonmail.com>
2021-04-03 22:18:53 -0300

    grid boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="grid">
+  <Image class="image" />
+  <Image class="image" />
+  <Image class="image" />
+  <Image class="image" />
+  <Image class="image" />
+  <Image class="image" />
+  <Image class="image" />
+  <Image class="image" />
+  <Image class="image" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Image from './components/Image.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Image
   }
 })
 </script>
 
 <style>
+#grid {
+  display: inline-grid;
+  grid-template-columns: repeat(3, 200px);
+  grid-template-rows: repeat(3, 200px);
+  column-gap: 5px;
+  row-gap: 5px;
+}
+
+.image {
+  border: 1px solid black;
+  vertical-align: auto;
+  line-height: 200px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Image.vue
@@ -0,0 +1,20 @@
+<template>
+<a href="" @click.prevent="test">Select image</a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+        test() {
+            console.log("hello world");
+        }
+    }
+})
+</script>
