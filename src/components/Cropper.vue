91c1c0ef27301baf0ea07a05701d4e9a8dd20427
Guilherme <gqgs@protonmail.com>
2021-04-16 03:27:27 -0300

    fix: avoid blurry output fix: destroy cropper on unmount fix: set zoom when component is ready

--- src/components/Cropper.vue
+++ src/components/Cropper.vue
@@ -1,7 +1,7 @@
 <template>
-    <VueCropper @cropend="cropend" ref="cropper"
+    <VueCropper @cropend="cropend" @ready="ready" ref="cropper"
       :title="result.title" :src="result.image_url"
-      rotatable=false scalable=false zoomable=false viewMode=1 aspectRatio=1 minCropBoxWidth=200 minCropBoxHeight=200
+      rotatable=false scalable=false zoomable=false viewMode=2 aspectRatio=1 minCropBoxWidth=200 minCropBoxHeight=200
     />
 </template>
 
@@ -25,13 +25,32 @@ export default defineComponent({
     ...mapActions([
       'updateCell'
     ]),
+    ready () {
+      /* eslint-disable-next-line no-unused-expressions */
+      (this.$refs.cropper as VueCropperMethods)?.zoomTo(0.5)
+    },
     cropend () {
-      const image = (this.$refs.cropper as VueCropperMethods).getCroppedCanvas().toDataURL()
-      this.updateCell({
-        image,
-        title: this.result.title
-      })
+      const { x, y, width, height } = (this.$refs.cropper as VueCropperMethods).getData()
+      const img = new Image()
+      img.onload = () => {
+        const canvas = document.createElement('canvas')
+        canvas.width = 200
+        canvas.height = 200
+        const ctx = canvas.getContext('2d')
+        /* eslint-disable-next-line no-unused-expressions */
+        ctx?.drawImage(img, x, y, width, height, 0, 0, 200, 200)
+        this.updateCell({
+          image: canvas.toDataURL('image/png'),
+          title: this.result.title
+        })
+      }
+      img.crossOrigin = 'Anonymous'
+      img.src = this.result.image_url
     }
+  },
+  unmounted () {
+    /* eslint-disable-next-line no-unused-expressions */
+    (this.$refs.cropper as VueCropperMethods)?.destroy()
   }
 })
 </script>
