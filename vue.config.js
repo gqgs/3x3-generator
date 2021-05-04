07df0629b23acaaff3abc800c2445fce8669a8fc
Guilherme <gqgs@protonmail.com>
2021-05-04 20:42:20 -0300

    fix(terser): remove comments from prod build

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,26 @@
 module.exports = {
   chainWebpack: config => {
     config.module
-      .rule('vue')
-      .use('vue-loader')
+      .rule("vue")
+      .use("vue-loader")
       .tap(options => {
         options.compilerOptions = {
           ...options.compilerOptions,
-          isCustomElement: tag => tag.startsWith('ion-')
+          isCustomElement: tag => tag.startsWith("ion-")
         }
         return options
       })
+
+    config.optimization.minimizer("terser").tap((args) => {
+      args[0].terserOptions.output = {
+        ...args[0].terserOptions.output,
+        comments: false
+      }
+      args[0].terserOptions.compress.drop_console = true
+      return args
+    })
   },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/3x3-generator/'
-    : '/'
+  publicPath: process.env.NODE_ENV === "production"
+    ? "/3x3-generator/"
+    : "/"
 }
