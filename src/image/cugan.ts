dd5802172f5e9addaab41750ea83d75c23ba5d5f
Guilherme <gqgs@protonmail.com>
2022-03-07 11:12:52 -0300

    upscaler(cugan): move all sync calls to tidy

--- src/image/cugan.ts
+++ src/image/cugan.ts
@@ -12,25 +12,22 @@ export default class Cugan2x implements UpscaleWorker {
 
   public async predict (pixels: ImageBitmap, canvas: HTMLCanvasElement | OffscreenCanvas | null = null): Promise<ImageBitmap> {
     const model = await this.model
-    const tidy = tf.tidy(() => {
+    const result = tf.tidy(() => {
       const img = tf.browser.fromPixels(pixels).toFloat()
       const offset = tf.scalar(255)
       const normalized = img.divNoNan(offset)
       const batched = normalized.reshape([1, 200, 200, 3])
       const pred = model.predict(batched) as tf.Tensor4D
       const tensor = pred.squeeze() as tf.Tensor3D
-      return tensor.clipByValue(0, 1)
+      return tensor.clipByValue(0, 1).as3D(332, 332, 3)
     })
 
     if (!canvas) {
       canvas = new OffscreenCanvas(332, 332)
     }
-    canvas.getContext("2d")
-    const as3D = tidy.as3D(332, 332, 3)
-    await tf.browser.toPixels(as3D, canvas as unknown as HTMLCanvasElement)
+    await tf.browser.toPixels(result, canvas as unknown as HTMLCanvasElement)
 
-    as3D.dispose()
-    tidy.dispose()
+    result.dispose()
     return createImageBitmap(canvas)
   }
 }
