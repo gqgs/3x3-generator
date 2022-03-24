75b55fad0c5521d2816ef1c1feb4394e260273d9
Guilherme <gqgs@protonmail.com>
2022-03-24 02:51:21 -0300

    feat(kitsu): add character tab

--- src/api/kitsu.ts
+++ src/api/kitsu.ts
@@ -2,10 +2,10 @@ import { SearchResult } from "../types"
 import { API } from "./api"
 
 interface APIResult {
-  data: Data[]
+  data: MediaData[] | CharacterData[]
 }
 
-interface Data {
+interface MediaData {
   id: number
   attributes: {
     titles: {
@@ -19,22 +19,48 @@ interface Data {
   }
 }
 
+interface CharacterData {
+  id: number
+  attributes: {
+    names: {
+      en: string
+    }
+    image?: {
+      original: string
+    }
+  }
+}
+
+const params = (tab: string, query: string): string => {
+  switch(tab) {
+    case "characters":
+      return `fields[${tab}]=names,image&filter[name]=${query}`
+    default:
+      return `fields[${tab}]=titles,posterImage&filter[text]=${query}`
+  }
+}
+
 export default class Kitsu extends API<APIResult> {
   readonly name = "kitsu"
-  readonly tabs = ["anime", "manga"]
+  readonly tabs = ["anime", "manga", "characters"]
 
   fetchURL(tab: string, query: string): { url: string } {
     return {
-      url: `https://kitsu.io/api/edge/${tab}?&fields[${tab}]=id,titles,posterImage&filter[text]=${encodeURI(query)}`
+      url: `https://kitsu.io/api/edge/${tab}?${params(tab, encodeURI(query))}`
     }
   }
   processResult(result: APIResult): SearchResult[] {
-    return (result.data ?? []).map((result: Data) => {
+    return (result.data ?? []).map((result: MediaData | CharacterData) => {
       return {
         mal_id: result.id,
-        title: result.attributes.titles.en_jp,
-        image_url: (result.attributes.posterImage.original || result.attributes.posterImage.large).replace("https://", "https://cdn.statically.io/img/")
+        title: isMediaData(result) ? result.attributes.titles.en_jp : result.attributes.names.en,
+        image_url: (isMediaData(result) ? (result.attributes.posterImage.original || result.attributes.posterImage.large) : result.attributes?.image?.original ?? "")
+          .replace("https://", "https://cdn.statically.io/img/")
       }
-    })
+    }).filter(result => result.image_url.length > 0)
   }
+}
+
+function isMediaData(data: MediaData | CharacterData): data is MediaData {
+  return (data as MediaData).attributes.titles !== undefined
 }
