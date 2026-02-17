06463e509859c1c5c9d6e2e5b90b682dd0b08870
Guilherme <gqgs@protonmail.com>
2026-02-17 16:45:28 -0300

    fix: multiple proxies

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,18 @@
 
+const proxies = [
+  "d16oqhcwx61zqh.cloudfront.net",
+  "d18oynqa97z70b.cloudfront.net",
+  "d1j7g7lrvvlssb.cloudfront.net",
+]
+
+const randomDNS = (): string => {
+  const randomIndex = Math.floor(Math.random() * proxies.length);
+  return proxies[randomIndex];
+}
+
 export const proxyImage = (url: string): string => {
   const parsed = URL.parse(url)
   if (!parsed) throw Error("failed to parse URL")
-  parsed.host = "d16oqhcwx61zqh.cloudfront.net"
+  parsed.host = randomDNS()
   return parsed.toString()
 }
